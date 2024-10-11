3fa5aabd5b9373d5a9e5187b43a51eb030b0f34c
Vegard Kyrkjedelen Hovland <vegard@kyrkjedelen.no>
2024-10-11 13:03:50 +0200

    refactor: :recycle: Update json parsing
    Updated json parsing from record(any) to a more detailed json validator, got this from the zod documentation

--- src/httpResponse.ts
+++ src/httpResponse.ts
@@ -1,7 +1,16 @@
 import { z } from "zod";
 import { RequestHandler } from "express";
 
-const HTTPResponseSchema = z.record(z.any()).transform(sch => {
+// From zod.dev (ggoodman)
+const literalSchema = z.union([z.string(), z.number(), z.boolean(), z.null()]);
+type Literal = z.infer<typeof literalSchema>;
+type Json = Literal | { [key: string]: Json } | Json[];
+const jsonSchema: z.ZodType<Json> = z.lazy(() =>
+  z.union([literalSchema, z.array(jsonSchema), z.record(jsonSchema)])
+);
+
+
+const HTTPResponseSchema = jsonSchema.transform(sch => {
     return {
         data: sch,
     }
