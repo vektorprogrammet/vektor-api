ec6129681124b73bb38ec7e7394d88e9e81074f9
Vegard Kyrkjedelen Hovland <vegard@kyrkjedelen.no>
2024-10-22 21:30:20 +0200

    refactor: :alembic: Add more robust database parameter parsing
    Added option to import SLL_OPTION from enviroment

--- db/config/parameters.ts
+++ db/config/parameters.ts
@@ -12,6 +12,18 @@ const databaseConnectionParametersSchema = z
 		DATABASE_USER: z.string().min(1),
 		DATABASE_PASSWORD: z.string().min(1),
 		DATABASE_PORT: z.coerce.number().positive().finite().safe().int(),
+		SSL_OPTION: z.union([
+			z.literal("true").transform(() => {
+				return true;
+			}),
+			z.literal("false").transform(() => {
+				return false;
+			}),
+			z.literal("require"),
+			z.literal("allow"),
+			z.literal("prefer"),
+			z.literal("verify-full"),
+		]),
 	})
 	.transform((schema) => {
 		return {
@@ -20,6 +32,7 @@ const databaseConnectionParametersSchema = z
 			user: schema.DATABASE_USER.trim(),
 			password: schema.DATABASE_PASSWORD.trim(),
 			port: schema.DATABASE_PORT,
+			ssl: schema.SSL_OPTION,
 		};
 	});
 
@@ -39,10 +52,4 @@ type DatabaseConnectionOptions = z.infer<
 export const databaseConnectionParameters: DatabaseConnectionOptions =
 	parsedEnviromentVariables;
 
-// Types defined at https://orm.drizzle.team/kit-docs/config-reference#dbcredentials
-const sslSetting: boolean | "require" | "allow" | "prefer" | "verify-full" =
-	"require";
-export const drizzleDatabaseCredentials = Object.assign(
-	{ ssl: sslSetting },
-	databaseConnectionParameters,
-);
+export const drizzleDatabaseCredentials = databaseConnectionParameters;
