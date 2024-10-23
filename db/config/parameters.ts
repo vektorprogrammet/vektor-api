bdc29cd47555cb02aaae97580699b4a9c7c916c1
Vegard Kyrkjedelen Hovland <vegard@kyrkjedelen.no>
2024-10-23 11:40:53 +0200

    feat: :sparkles: Add default behaviour for SSL_OPTION
    The default is false

--- db/config/parameters.ts
+++ db/config/parameters.ts
@@ -13,35 +13,37 @@ export const databaseConnectionParameters = z
 		DATABASE_USER: z.string().min(1),
 		DATABASE_PASSWORD: z.string().min(1),
 		DATABASE_PORT: z.coerce.number().positive().finite().safe().int(),
-		SSL_OPTION: z.union([
-			z.literal("customOn").transform((_, ctx) => {
-				const ca_cert = getCaCert();
-				if (ca_cert === undefined) {
-					ctx.addIssue({
-						code: z.ZodIssueCode.custom,
-						message: "Could not find ca certificate",
-					});
-					return z.NEVER;
-				}
-				return {
-					requestCert: true,
-					rejectUnauthorized: true,
-					ca: ca_cert,
-				} as ConnectionOptions;
-			}),
-			z.literal("customOff").transform(() => {
-				return {
-					requestCert: false,
-					rejectUnauthorized: false,
-				} as ConnectionOptions;
-			}),
-			z.literal("true").transform(() => {
-				return true;
-			}),
-			z.literal("false").transform(() => {
-				return false;
-			}),
-		]),
+		SSL_OPTION: z
+			.union([
+				z.literal("customOn").transform((_, ctx) => {
+					const ca_cert = getCaCert();
+					if (ca_cert === undefined) {
+						ctx.addIssue({
+							code: z.ZodIssueCode.custom,
+							message: "Could not find ca certificate",
+						});
+						return z.NEVER;
+					}
+					return {
+						requestCert: true,
+						rejectUnauthorized: true,
+						ca: ca_cert,
+					} as ConnectionOptions;
+				}),
+				z.literal("customOff").transform(() => {
+					return {
+						requestCert: false,
+						rejectUnauthorized: false,
+					} as ConnectionOptions;
+				}),
+				z.literal("true").transform(() => {
+					return true;
+				}),
+				z.literal("false").transform(() => {
+					return false;
+				}),
+			])
+			.default("false"),
 	})
 	.transform((schema) => {
 		return {
