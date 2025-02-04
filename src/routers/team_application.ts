c0e9c47b7d4500ee4c517cee2f5456b7f3a32903
solvhold <solvhold@stud.ntnu.no>
2025-02-04 14:46:30 +0100

    feat: :sparkles: create routers for getting team applications
    one router for all team applications with limit and offset, and one for selecting teamapplications given a team id
    
    vf-194

--- src/routers/team_application.ts
+++ src/routers/team_application.ts
@@ -1,5 +1,39 @@
+import {
+	selectTeamApplications,
+	selectTeamApplicationsByTeamId,
+} from "@src/db-access/team_applications";
+import { clientError } from "@src/error/httpErrors";
+import { idValidator, queryValidator } from "@src/request-handling/common";
+import { teamApplicationValidator } from "@src/request-handling/team_application";
 import { Router, urlencoded } from "express";
 
-const teamApplicationRouter = Router()
+export const teamApplicationRouter = Router();
 
 teamApplicationRouter.use(urlencoded({ extended: true }));
+
+teamApplicationRouter.get("/", async (req, res, next) => {
+	const queryParametersResult = queryValidator.safeParse(req.query);
+	if (!queryParametersResult.success) {
+		return next(clientError(400, "", queryParametersResult.error));
+	}
+
+	const results = await selectTeamApplications(queryParametersResult.data);
+	if (!results.success) {
+		return next(clientError(400, "Database error", results.error));
+	}
+	res.json(results.data);
+});
+
+teamApplicationRouter.get("/:teamID/", async (req, res, next) => {
+	const teamIdResult = idValidator.safeParse(req.params.teamID);
+	if (!teamIdResult.success) {
+		return next(clientError(400, "", teamIdResult.error));
+	}
+	const databaseResult = await selectTeamApplicationsByTeamId([
+		teamIdResult.data,
+	]);
+	if (!databaseResult.success) {
+		return next(clientError(400, "Database error", databaseResult.error));
+	}
+	res.json(databaseResult.data);
+});
