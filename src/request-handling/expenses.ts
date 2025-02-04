e0802211d57142cd12e7b9c0be63ab41d47186a6
Vegard Kyrkjedelen Hovland <vegard@kyrkjedelen.no>
2025-02-04 15:07:28 +0100

    refactor: Update expenseRequest parsing
    Moved from a Transformer approch to a extend approch, because it will fit better with new lib parsers

--- src/request-handling/expenses.ts
+++ src/request-handling/expenses.ts
@@ -1,61 +1,29 @@
-import { ceilToTwoDecimals } from "@lib/moneyParser";
-import { removeSeparatorsNorwegianAccountNumberNoIBAN } from "@lib/moneyParser";
+import { expensesTable } from "@db/tables/expenses";
+import { accountNumberParser, currencyParser } from "@lib/financeParsers";
+import { serialIdParser } from "@src/request-handling/common";
+import { createInsertSchema } from "drizzle-zod";
 import { z } from "zod";
 
-export const expenseRequestValidator = z.object({
-	userId: z.coerce
-		.number()
-		.finite()
-		.safe()
-		.positive()
-		.describe("Id of user requesting expense"),
-	title: z.string().min(1).describe("Title of expense"),
-	moneyAmount: z.coerce
-		.number()
-		.finite()
-		.safe()
-		.positive()
-		.describe("Amount of money used"),
-	description: z.string().min(1).describe("Description of expense"),
-	accountNumber: z.string().min(11).describe("Norwegian account number"),
-	purchaseDate: z
-		.string()
-		.date("Must be valid datestring (YYYY-MM-DD)")
-		.describe("Date of purcase"),
-});
-export const expenseRequestTransformer = expenseRequestValidator.transform(
-	(schema, ctx) => {
-		return {
-			userId: schema.userId,
-			title: schema.title.trim(),
-			moneyAmount: (() => {
-				const result = ceilToTwoDecimals(schema.moneyAmount);
-				if (result.success) {
-					return String(result.data);
-				}
-				ctx.addIssue({
-					code: z.ZodIssueCode.custom,
-					message: result.error.message,
-				});
-				return z.NEVER;
-			})(),
-			description: schema.description.trim(),
-			accountNumber: (() => {
-				try {
-					return removeSeparatorsNorwegianAccountNumberNoIBAN(
-						schema.accountNumber,
-					);
-				} catch (e) {
-					ctx.addIssue({
-						code: z.ZodIssueCode.custom,
-						message: (e as Error).message,
-					});
-					return z.NEVER;
-				}
-			})(),
-			purchaseDate: (() => {
-				return new Date(schema.purchaseDate);
-			})(),
-		};
-	},
-);
+export const expenseRequestParser = z
+	.object({
+		userId: serialIdParser.describe("Id of user requesting expense"),
+		title: z.string().min(1).describe("Title of expense"),
+		moneyAmount: currencyParser.describe("Amount of money used"),
+		description: z.string().min(1).describe("Description of expense"),
+		accountNumber: accountNumberParser.describe("Norwegian account number"),
+		purchaseDate: z
+			.string()
+			.date("Must be valid datestring (YYYY-MM-DD)")
+			.describe("Date of purcase"),
+	})
+	.strict();
+export const expenseRequestToInsertParser = expenseRequestParser
+	.extend({
+		title: expenseRequestParser.shape.title.trim(),
+		description: expenseRequestParser.shape.description.trim(),
+		purchaseDate: expenseRequestParser.shape.purchaseDate.pipe(z.coerce.date()),
+	})
+	.pipe(createInsertSchema(expensesTable))
+	.readonly();
+
+export type NewExpense = z.infer<typeof expenseRequestToInsertParser>;
