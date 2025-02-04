7f670aa36f9ad98df176ae5a97203f6d58af87e2
solvhold <solvhold@stud.ntnu.no>
2025-02-04 14:43:25 +0100

    feat: db-access for teamapplications
    vf-194

--- src/db-access/team_applications.ts
+++ src/db-access/team_applications.ts
@@ -1,19 +1,49 @@
-import { teamApplicationsTable } from "@db/tables/teamApplication";
 import { database } from "@db/setup/queryPostgres";
-import { catchDatabase, DatabaseResult } from "@src/error/ormError";
-import { asc } from 'drizzle-orm';
-import { TeamApplication } from "@src/response-handling/team_application";
+import { teamApplicationsTable } from "@db/tables/teamApplication";
+import {
+	type DatabaseResult,
+	catchDatabase,
+	handleDatabaseFullfillment,
+	handleDatabaseRejection,
+	ormError,
+} from "@src/error/ormError";
+import type { QueryParameters } from "@src/request-handling/common";
+import type {
+	TeamApplication,
+	TeamKey,
+} from "@src/response-handling/team_application";
+import { asc, inArray } from "drizzle-orm";
 
-export const getTeamApplications = async (
-    limit: number
+export const selectTeamApplications = async (
+	parameters: QueryParameters,
 ): Promise<DatabaseResult<TeamApplication[]>> => {
-    return catchDatabase( async () => {
-        return await database
-        .select()
-        .from(teamApplicationsTable)
-        .orderBy(asc(teamApplicationsTable.id))
-        .limit(limit)
-        }
+	return catchDatabase(async () => {
+		return await database
+			.select()
+			.from(teamApplicationsTable)
+			.orderBy(asc(teamApplicationsTable.id))
+			.limit(parameters.limit)
+			.offset(parameters.offset);
+	});
+};
 
-    )
-}
+export const selectTeamApplicationsByTeamId = async (
+	teamId: TeamKey[],
+): Promise<DatabaseResult<TeamApplication[]>> => {
+	return database
+		.transaction(async (tx) => {
+			const selectResult = await tx
+				.select()
+				.from(teamApplicationsTable)
+				.where(inArray(teamApplicationsTable.teamId, teamId));
+			if (selectResult.length == teamId.length) {
+                return selectResult;
+				
+			}
+            throw ormError(
+                "Couldn't select team applications, id's (team) didn't exist.",
+            );
+			
+		})
+		.then(handleDatabaseFullfillment, handleDatabaseRejection);
+};
