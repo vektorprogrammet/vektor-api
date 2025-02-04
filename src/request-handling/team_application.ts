c2cf5c93513c79c88be83d44328eaf2600087cea
solvhold <solvhold@stud.ntnu.no>
2025-02-04 14:44:19 +0100

    feat: request handling for teamapplications
    validate the object using zod
    
    vf-194

--- src/request-handling/team_application.ts
+++ src/request-handling/team_application.ts
@@ -1,13 +1,49 @@
+import { maxTextLength } from "@lib/globalVariables";
 import { z } from "zod";
 
-const teamApplicationSchema = z.object({
-    id: z.coerce.number(),
-	teamId: z.coerce.number(),
-	name: z.string(),
-	email: z.string().email(),
-	motivationText: z.string(),
-	fieldOfStudy: z.string(),
-	yearOfStudy: z.number(),
-	biography: z.string(),
-	phonenumber: z.string().regex( /^\d{8}$/, "Phone number must be 8 digits"),
-}).pipe
+export const teamApplicationValidator = z.object({
+	id: z.coerce
+		.number()
+		.finite()
+		.safe()
+		.positive()
+		.describe("Id of team application requested"),
+	teamId: z.coerce
+		.number()
+		.finite()
+		.safe()
+		.positive()
+		.describe("Id of team applied for"),
+	name: z.string().min(1).describe("Name of user applying for a team"),
+	email: z
+		.string()
+		.trim()
+		.toLowerCase()
+		.email()
+		.describe("Email of user applying for a team"),
+	motivationText: z
+		.string()
+		.trim()
+		.max(maxTextLength)
+		.describe("The motivation text of user applying for a team"),
+	fieldOfStudy: z
+		.string()
+		.trim()
+		.max(maxTextLength)
+		.describe("Studyfield of user applying for a team"),
+	yearOfStudy: z
+		.number()
+		.finite()
+		.safe()
+		.positive()
+		.describe("The year of study the user applying for a team is in"),
+	biography: z
+		.string()
+		.trim()
+		.max(maxTextLength)
+		.describe("The biography of the user applying for a team"),
+	phonenumber: z
+		.string()
+		.regex(/^\d{8}$/, "Phone number must be 8 digits")
+		.describe("The phonenumber of the user applying for a team"),
+});
