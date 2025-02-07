1c7dfc13ef08566307bbf4001235668ee240faed
solvhold <solvhold@stud.ntnu.no>
2025-02-07 13:38:26 +0100

    feat: Split into two schemas: one to validate strings, and one to transform them to needed datatype
    vf-194

--- src/request-handling/team_application.ts
+++ src/request-handling/team_application.ts
@@ -1,34 +1,35 @@
+import { fieldsOfStudyTable } from "@db/tables/fieldsOfStudy";
+import { teamApplicationsTable } from "@db/tables/teamApplication";
 import { maxTextLength } from "@lib/globalVariables";
+import { createInsertSchema } from "drizzle-zod";
 import { z } from "zod";
 
-export const teamApplicationValidator = z.object({
-	id: z.coerce
+export const teamApplicationParser = z.object({
+	id: z
 		.number()
 		.finite()
 		.safe()
 		.positive()
+		.int()
 		.describe("Id of team application requested"),
-	teamId: z.coerce
+	teamId: z
 		.number()
 		.finite()
 		.safe()
 		.positive()
+		.int()
 		.describe("Id of team applied for"),
 	name: z.string().min(1).describe("Name of user applying for a team"),
 	email: z
 		.string()
-		.trim()
-		.toLowerCase()
 		.email()
 		.describe("Email of user applying for a team"),
 	motivationText: z
 		.string()
-		.trim()
 		.max(maxTextLength)
 		.describe("The motivation text of user applying for a team"),
 	fieldOfStudy: z
 		.string()
-		.trim()
 		.max(maxTextLength)
 		.describe("Studyfield of user applying for a team"),
 	yearOfStudy: z
@@ -36,10 +37,10 @@ export const teamApplicationValidator = z.object({
 		.finite()
 		.safe()
 		.positive()
+		.int()
 		.describe("The year of study the user applying for a team is in"),
 	biography: z
 		.string()
-		.trim()
 		.max(maxTextLength)
 		.describe("The biography of the user applying for a team"),
 	phonenumber: z
@@ -47,3 +48,14 @@ export const teamApplicationValidator = z.object({
 		.regex(/^\d{8}$/, "Phone number must be 8 digits")
 		.describe("The phonenumber of the user applying for a team"),
 });
+
+export const teamApplicationToInsertParser = teamApplicationParser.extend({
+	email: teamApplicationParser.shape.email.trim().toLowerCase(),
+	motivationText: teamApplicationParser.shape.motivationText.trim(),
+	fieldsOfStudy: teamApplicationParser.shape.fieldOfStudy.trim(),
+	biography: teamApplicationParser.shape.biography.trim(),
+}
+
+).pipe(createInsertSchema(teamApplicationsTable)).readonly()
+
+export type NewTeamApplication = z.infer<typeof teamApplicationToInsertParser>
