54ee0ff86344a414a8cfdf8f54d22a0633e886b6
solvhold <solvhold@stud.ntnu.no>
2025-02-17 18:28:21 +0100

    feat: add router for teamapplication post request
    vf-234

--- src/routers/team_application.ts
+++ src/routers/team_application.ts
@@ -1,14 +1,18 @@
 import {
+	insertTeamApplication,
 	selectTeamApplications,
 	selectTeamApplicationsByTeamId,
 } from "@src/db-access/team_applications";
 import { clientError } from "@src/error/httpErrors";
 import { listQueryParser, serialIdParser } from "@src/request-handling/common";
-import { Router, urlencoded } from "express";
+import { teamApplicationToInsertParser } from "@src/request-handling/team_application";
+import { json, Router, urlencoded } from "express";
 
 export const teamApplicationRouter = Router();
 
-teamApplicationRouter.use(urlencoded({ extended: true }));
+teamApplicationRouter.use(json());
+
+
 
 teamApplicationRouter.get("/", async (req, res, next) => {
 	const queryParametersResult = listQueryParser.safeParse(req.query);
@@ -40,4 +44,22 @@ teamApplicationRouter.get("/:teamID/", async (req, res, next) => {
 		return next(clientError(400, "Database error", databaseResult.error));
 	}
 	res.json(databaseResult.data);
+});
+
+teamApplicationRouter.post("/", async (req, res, next) => {
+	const teamApplicationReq = teamApplicationToInsertParser.safeParse(req.body);
+	if (!teamApplicationReq.success) {
+		const error = clientError(
+			400,
+			"Failed parsing teamapplication request.",
+			teamApplicationReq.error,
+		);
+		return next(error);
+	}
+	const databaseResult = await insertTeamApplication([teamApplicationReq.data]);
+	if (!databaseResult.success) {
+		const error = clientError(400, "Database error", databaseResult.error);
+		return next(error);
+	}
+	res.status(201).json(teamApplicationReq.data);
 });
