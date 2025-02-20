fab18fe05409ba4a21019bd386259485b7a0831d
Vegard Kyrkjedelen Hovland <vegard@kyrkjedelen.no>
2025-02-20 18:27:32 +0100

    style: :rotating_light: Fix small linting warning

--- src/response-handling/team_application.ts
+++ src/response-handling/team_application.ts
@@ -3,9 +3,9 @@ import type { z } from "zod";
 
 import { teamApplicationsTable } from "@db/tables/teamApplication";
 
-export const expensesSelectSchema = createSelectSchema(
-	teamApplicationsTable,
-).strict().readonly();
+export const expensesSelectSchema = createSelectSchema(teamApplicationsTable)
+	.strict()
+	.readonly();
 
 export type TeamApplication = z.infer<typeof expensesSelectSchema>;
 export type TeamApplicationKey = TeamApplication["id"];
