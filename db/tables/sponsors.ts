7c8fdfe891a72995309b898c757f2776913b80c8
TobiasH05 <tobiashegge05@gmail.com>
2025-02-27 11:18:35 +0100

    feat: :sparkles: Added table for sponsors

---
+++ db/tables/sponsors.ts
@@ -0,0 +1,30 @@
+import { relations } from "drizzle-orm";
+import { departmentsTable } from "./departments";
+import mainSchema from "./schema";
+import { date, integer, serial, text } from "drizzle-orm/pg-core";
+
+export const sponsorSizeEnum = mainSchema.enum("size", [
+    "small",
+    "medium",
+    "large"
+]);
+
+export const sponsorsTable = mainSchema.table("sponsors", {
+    id: serial("id").primaryKey(),
+    name: text("name").notNull(),
+    homePageURL: text("homePageURL").notNull(),
+    startDate: date("startDate", { mode: "date" }).notNull(),
+    endDate: date("endDate", { mode: "date" }),
+    size: sponsorSizeEnum("size").notNull(),
+    spesificDepartmentId: integer("spesificDepartmentId").references(() => departmentsTable.id)
+});
+
+export const sponsorsRelations = relations(
+    sponsorsTable,
+    ({ one }) => ({
+        department: one(departmentsTable, {
+            fields: [sponsorsTable.spesificDepartmentId],
+            references: [departmentsTable.id]
+        })
+    }),
+)
