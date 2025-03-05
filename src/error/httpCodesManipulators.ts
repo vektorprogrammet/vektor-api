a9e04edd312412b2668f8bc0d2b0396ecc0c6261
Vegard Kyrkjedelen Hovland <vegard@kyrkjedelen.no>
2025-03-05 11:46:40 +0100

    feat: :sparkles: Add manipulators for http codes
    Add manipulators to validate and transform http codes into info-objects

---
+++ src/error/httpCodesManipulators.ts
@@ -0,0 +1,60 @@
+import { HTTP_STATUS_CODE_MAP } from "./httpCodes";
+
+export type HttpClientErrorCode =
+	keyof (typeof HTTP_STATUS_CODE_MAP)["Client error"];
+export type HttpServerErrorCode =
+	keyof (typeof HTTP_STATUS_CODE_MAP)["Server error"];
+export type HttpErrorCode = HttpClientErrorCode | HttpServerErrorCode;
+
+function isHttpClientErrorCode(code: number): code is HttpClientErrorCode {
+	return Object.keys(HTTP_STATUS_CODE_MAP["Client error"])
+		.map(Number)
+		.includes(code);
+}
+function isHttpServerErrorCode(code: number): code is HttpServerErrorCode {
+	return Object.keys(HTTP_STATUS_CODE_MAP["Server error"])
+		.map(Number)
+		.includes(code);
+}
+function isHttpErrorCode(code: number) {
+	return isHttpClientErrorCode(code) || isHttpServerErrorCode(code);
+}
+
+export type HttpClientErrorCodeInfo = {
+	title: "Client error";
+	code: HttpClientErrorCode;
+	message: (typeof HTTP_STATUS_CODE_MAP)["Client error"][HttpClientErrorCode];
+};
+export type HttpServerErrorCodeInfo = {
+	title: "Server error";
+	code: HttpServerErrorCode;
+	message: (typeof HTTP_STATUS_CODE_MAP)["Server error"][HttpServerErrorCode];
+};
+export type HttpErrorCodeInfo =
+	| HttpClientErrorCodeInfo
+	| HttpServerErrorCodeInfo;
+
+export function getHttpClientErrorCodeInfo(
+	code: HttpClientErrorCode,
+): HttpClientErrorCodeInfo {
+	return {
+		title: "Client error",
+		code: code,
+		message: HTTP_STATUS_CODE_MAP["Client error"][code],
+	};
+}
+export function getHttpServerErrorCodeInfo(
+	code: HttpServerErrorCode,
+): HttpServerErrorCodeInfo {
+	return {
+		title: "Server error",
+		code: code,
+		message: HTTP_STATUS_CODE_MAP["Server error"][code],
+	};
+}
+export function getHttpErrorCodeInfo(code: HttpErrorCode): HttpErrorCodeInfo {
+	if (isHttpClientErrorCode(code)) {
+		return getHttpClientErrorCodeInfo(code);
+	}
+	return getHttpServerErrorCodeInfo(code);
+}
